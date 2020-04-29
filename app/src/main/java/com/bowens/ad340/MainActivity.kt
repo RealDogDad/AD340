a1c75b994f30cdd275ead9d96748e214229f1e80
B.Foster <owensbrandan@gmail.com>
2020-04-29 15:58:36 -0700

    Updated to add ZipCode and Submit Button Small Color UI change

--- app/src/main/java/com/bowens/ad340/MainActivity.kt
+++ app/src/main/java/com/bowens/ad340/MainActivity.kt
@@ -2,11 +2,28 @@ package com.bowens.ad340
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var zipcodeEditText: EditText = findViewById(R.id.zip)
+        var submitButton: Button = findViewById(R.id.submit)
+
+        submitButton.setOnClickListener {
+            val zipCode: String = zipcodeEditText.text.toString()
+            if(zipCode.length != 5) {
+                Toast.makeText(this, "Please enter valid Zip Code", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, zipCode, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
