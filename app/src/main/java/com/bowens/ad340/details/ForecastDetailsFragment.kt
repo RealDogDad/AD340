2b2b833baaa3032ea085e86da12d5e5b5c9adecb
Prefabmango112 <owensbrandan@gmail.com>
2020-06-17 15:27:27 -0700

    Finished Assignment 7

--- app/src/main/java/com/bowens/ad340/details/ForecastDetailsFragment.kt
+++ app/src/main/java/com/bowens/ad340/details/ForecastDetailsFragment.kt
@@ -1,18 +1,24 @@
 package com.bowens.ad340.details
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bowens.ad340.R
 import com.bowens.ad340.TempDisplaySettingManager
+import com.bowens.ad340.databinding.FragmentForecastDetailsBinding
 import com.bowens.ad340.formatTempForDisplay
-import com.bowens.ad340.showTempDisplaySettingDialog
 
 class ForecastDetailsFragment : Fragment() {
     private val args: ForecastDetailsFragmentArgs by navArgs()
+
+    private var _binding: FragmentForecastDetailsBinding? = null
+    // This property only valid between onCreateView and onDestroyView
+    private val binding get()= _binding!!
+
     private lateinit var tempDisplaySettingManager: TempDisplaySettingManager
 
     override fun onCreateView(
@@ -20,8 +26,10 @@ class ForecastDetailsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val layout = inflater.inflate(R.layout.fragment_forecast_details, container, false)
+        _binding = FragmentForecastDetailsBinding.inflate(inflater, container, false)
         tempDisplaySettingManager = TempDisplaySettingManager(requireContext())
+
+
         val tempText = layout.findViewById<TextView>(R.id.tempText)
         val descriptionText = layout.findViewById<TextView>(R.id.descriptionText)
 
